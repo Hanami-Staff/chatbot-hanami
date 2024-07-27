ccb09ed63b5a194b1ea18c71894641b7fe87ecd7
Adrian Roger <adrianroge@gmail.com>
2024-07-26 21:16:50 -0300

    feat: create startchat component

---
+++ src/components/StartChat.jsx
@@ -0,0 +1,35 @@
+import { functionalities } from "../constants";
+import { ChatInput, FunctionalityCard } from '../components'
+
+
+const StartChat = () => {
+
+  return (
+    <div className="mx-4 bg-[#222222] rounded-lg h-full bg-late-500 relative overflow-hidden border border-white/15">
+      <div className="size-[1528px] bg-gradient-to-tr from-[#FA6CD2CC] to-[#222222] filter blur-3xl  rounded-full absolute bottom-0 -translate-x-1/2 translate-y-1/2" />
+
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="text-center text absolute w-[70%] max-w-[800px]">
+          <h1 className="text-5xl font-semibold">
+            Bem-vindo(a) ao <span className="span">HanaBot</span>
+          </h1>
+
+          <p className="mt-4 mb-[68px]">
+            Floresça seu conhecimento em lógica e programação com o poder da IA.{" "}
+            {`Diga: "Olá aluno, eu sou o HanaBot, em que posso ajudá-lo"`}
+          </p>
+
+          <ChatInput />
+
+          <div className="flex gap-12 pt-[104px] justify-center">
+            {functionalities.map((functionality, i) => (
+              <FunctionalityCard key={i} {...functionality} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartChat
